45dde8002561974d1fb4737b8d7c3e9bfb16dce7
meza <mohammedhimmet3@gmail.com>
2020-07-06 18:11:05 +0300

    #Bu repoya attigim ilk kod..

---
+++ Assign_9.py
@@ -0,0 +1,8 @@
+my_name = "muhammed"
+name = input("Name : ")
+
+if name.strip().lower() == my_name:
+    print("Hello Muhammed! Your Password is: qweas!d12")
+
+else :
+    print(f"Hello {name.title()}! See you later!")
